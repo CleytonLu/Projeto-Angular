ff705540099df43ac79a8fc528c2625ba3f07db8
CleytonLu <cleyton.cabral>
2024-07-24 16:29:05 -0300

    Projeo angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular_documentacao';
+  title = 'Inicio de tudo...';
 }
